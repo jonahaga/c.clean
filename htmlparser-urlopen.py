025cb1f21ece328e3403c785dc4b54cad0b697cb
Jona Haga-Camacho <jona.haga@gmail.com>
2014-03-20 15:46:06 -0700

    Parses CSS and HTML and returns the CSS selectors that aren't being used in HTML

--- htmlparser-urlopen.py
+++ htmlparser-urlopen.py
@@ -1,6 +1,7 @@
 from HTMLParser import HTMLParser
 import urllib
 import cssutils
+import re
 
 
 opener = urllib.FancyURLopener({})
@@ -14,32 +15,40 @@ class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         ignore = ['html', 'head', 'title', 'link', 'style', 'script', 'meta']
         if tag not in ignore:
-            if attrs and attrs[0][0] != 'href' and attrs[0][0] == 'class':
-                if attrs and ' ' in attrs[0][1]:
-                    split_attrs = attrs[0][1].split()
-                    self.selectors.append(tag + '.' + split_attrs[0])
-                    self.selectors.append(tag + '.' + split_attrs[1])
-                    self.selectors.append(tag + '.' + split_attrs[0] + '.' + split_attrs[1])
-                    self.selectors.append('.' + split_attrs[0])
-                    self.selectors.append('.' + split_attrs[1])
-                    self.selectors.append('.' + split_attrs[0] + '.' + split_attrs[1])
+            for i in attrs:
+                if i[0] == 'class':
+                    class_index = attrs.index(i)
+                    
+                    if attrs and attrs[class_index][0] == 'class': #or attrs[1][0]:
+                        if attrs and ' ' in attrs[class_index][1]:
+                            split_attrs = attrs[class_index][1].split()
+                            self.selectors.append(tag + '.' + split_attrs[0])
+                            self.selectors.append(tag + '.' + split_attrs[1])
+                            self.selectors.append(tag + '.' + split_attrs[0] + '.' + split_attrs[1])
+                            self.selectors.append('.' + split_attrs[0])
+                            self.selectors.append('.' + split_attrs[1])
+                            self.selectors.append('.' + split_attrs[0] + '.' + split_attrs[1])
+                        else:
+                            self.selectors.append('.' + attrs[class_index][1])
+                            self.selectors.append(tag + '.' + attrs[class_index][1])
+
+                elif i[0] == 'id':
+                    id_index = attrs.index(i)
+                    
+                    if attrs and attrs[id_index][0] == 'id':
+                        if attrs and ' ' in attrs[id_index][1]:
+                            split_attrs = attrs[id_index][1].split()
+                            self.selectors.append(tag + '#' + split_attrs[0])
+                            self.selectors.append(tag + '#' + split_attrs[1])
+                            self.selectors.append(tag + '#' + split_attrs[0] + '#' + split_attrs[1])
+                            self.selectors.append('#' + split_attrs[0])
+                            self.selectors.append('#' + split_attrs[1])
+                            self.selectors.append('#' + split_attrs[0] + '#' + split_attrs[1])
+                        else:
+                            self.selectors.append('#' + attrs[id_index][1])
+                            self.selectors.append(tag + '#' + attrs[id_index][1])
                 else:
-                    self.selectors.append('.' + attrs[0][1])
-                    self.selectors.append(tag + '.' + attrs[0][1])
-            elif attrs and attrs[0][0] != 'href' and attrs[0][0] == 'id':
-                if attrs and ' ' in attrs[0][1]:
-                    split_attrs = attrs[0][1].split()
-                    self.selectors.append(tag + '#' + split_attrs[0])
-                    self.selectors.append(tag + '#' + split_attrs[1])
-                    self.selectors.append(tag + '#' + split_attrs[0] + '#' + split_attrs[1])
-                    self.selectors.append('#' + split_attrs[0])
-                    self.selectors.append('#' + split_attrs[1])
-                    self.selectors.append('#' + split_attrs[0] + '#' + split_attrs[1])
-                else:
-                    self.selectors.append('#' + attrs[0][1])
-                    self.selectors.append(tag + '#' + attrs[0][1])
-            else:
-                self.selectors.append(tag)
+                    self.selectors.append(tag)
 
 def compile_files(files_to_parse):
     file_list = files_to_parse.split(',')
@@ -48,66 +57,99 @@ def compile_files(files_to_parse):
     for i in file_list:
         stripped = i.strip()
         f = opener.open(stripped, 'r')
-        h = f.read()
+        n = f.read()
 
-        new_file += h
+        new_file += n
 
         f.close()
 
     return new_file
 
+css_file = cssutils.CSSParser().parseString(compile_files(css_to_parse))
+
 def css_parser(files_to_parse):
-    css_selectors = []
-    css_file = cssutils.CSSParser().parseString(compile_files(files_to_parse))
+    css_dict = {}
 
     for i in css_file.cssRules:
         if not isinstance(i, cssutils.css.CSSComment) and not isinstance(i, cssutils.css.CSSImportRule):
             for j in i.selectorList:
-                if ' ' in j.selectorText:
+                p = re.compile('\.\w*(\.)\w*')
+
+                if p.match(str(j.selectorText)) and ' ' not in j.selectorText:
+                    dot_combinator = re.split(('(\.\w*)(\.\w*)', str(j.selectorText)))
+                    
+                    for m in dot_combinator:
+                        if m == '':
+                            pass
+                        else:
+                            css_dict.update({m:j})
+
+                elif ' ' in j.selectorText:
                     combinator = str(j.selectorText)
                     split_combinator = combinator.split()
+
                     for k in split_combinator:
-                        if k != '>':
-                            css_selectors.append(k)
+                        if k == '>' or k == '+' or k == '~':
+                            pass
+                        elif p.match(k):
+                            dot_split = re.split('(\.\w*)(\.\w*)', k)
+                            for s in dot_split:
+                                if s == '':
+                                    pass
+                                else:
+                                    css_dict.update({s:j})
+                        else:
+                            css_dict.update({k:j})               
+                
+                elif '*' in j.selectorText:
+                    pass
                 else:
-                    css_selectors.append(str(j.selectorText))
+                    css_dict.update({str(j.selectorText):j})
 
-    return css_selectors
+    return css_dict
 
-def split_pseudo(l):
-    css_selectors = []
+def split_pseudo(d):
+    css_dict = {}
 
-    for i in l:
-        if ':' in i:
-            split_pseudo = i.split(':')
+    for key in d:
+        if ':' in key:
+            split_pseudo = key.split(':')
             if '[' not in split_pseudo[0]:
-                css_selectors.append(split_pseudo[0])
+                css_dict.update({split_pseudo[0]: css_parser(css_file).get(key)})
             else:
                 attribute_selector = split_pseudo[0].split('[')
-                css_selectors.append(attribute_selector[0])
+                css_dict.update({attribute_selector[0]: css_parser(css_file).get(key)})
         else:
-            css_selectors.append(i)
+            css_dict.update({key: css_parser(css_file).get(key)})
 
-    return css_selectors
+    return css_dict
 
 def del_dupes(l):
-    n = []
+    c = {}
+    h = []
 
-    for i in l:
-        if i not in n:
-            n.append(i)
-    return n
+    if type(l) is dict:
+        for key in l:
+            if key not in c:
+                c.update({key: l.get(key)})
+        return c
+    else:
+        for i in l:
+            if i not in h:
+                h.append(i)
+        return h
 
 def compare_selectors():
-    for i in css_selectors:
-        if i not in del_dupes(parser.selectors):
-            print i, 'is not used'
+    for key in del_dupes(split_pseudo(css_parser(css_file))):
+        if key not in del_dupes(parser.selectors):
+            print key, 'is not used'
 
-# instantiate the parser and fed it some HTML
+
+# instantiate the parser and feed it some HTML
 parser = MyHTMLParser()
 parser.feed(compile_files(html_to_parse))
-css_selectors = del_dupes(split_pseudo(css_parser(css_to_parse)))
-# print del_dupes(parser.selectors)
-print compare_selectors()
 parser.close()
+
+# instantiate the css parser and feed it some CSS
+print compare_selectors()
 
