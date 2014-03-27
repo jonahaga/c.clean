bed091f14ada96ecc3d0f02947b4f5e9f3205fec
Jona Haga-Camacho <jona.haga@gmail.com>
2014-03-27 13:33:25 -0700

    Switched dictionary to list of tuples

--- cssclean.py
+++ cssclean.py
@@ -8,42 +8,45 @@ from urllib2 import urlopen
 class MyHTMLParser(HTMLParser):
     selectors = []
     def handle_starttag(self, tag, attrs):
-        ignore = ['html', 'head', 'title', 'link', 'style', 'script', 'meta']
+        ignore = ['head', 'title', 'link', 'style', 'script', 'meta']
         if tag not in ignore:
-            for i in attrs:
-                if i[0] == 'class':
-                    class_index = attrs.index(i)
-                    
-                    if attrs and attrs[class_index][0] == 'class': #or attrs[1][0]:
-                        if attrs and ' ' in attrs[class_index][1]:
-                            split_attrs = attrs[class_index][1].split()
-                            self.selectors.append(tag + '.' + split_attrs[0])
-                            self.selectors.append(tag + '.' + split_attrs[1])
-                            self.selectors.append(tag + '.' + split_attrs[0] + '.' + split_attrs[1])
-                            self.selectors.append('.' + split_attrs[0])
-                            self.selectors.append('.' + split_attrs[1])
-                            self.selectors.append('.' + split_attrs[0] + '.' + split_attrs[1])
-                        else:
-                            self.selectors.append('.' + attrs[class_index][1])
-                            self.selectors.append(tag + '.' + attrs[class_index][1])
+            if attrs:
+                for i in attrs:
+                    if i[0] == 'class':
+                        class_index = attrs.index(i)
+                        
+                        if attrs and attrs[class_index][0] == 'class': #or attrs[1][0]:
+                            if attrs and ' ' in attrs[class_index][1]:
+                                split_attrs = attrs[class_index][1].split()
+                                self.selectors.append(tag + '.' + split_attrs[0])
+                                self.selectors.append(tag + '.' + split_attrs[1])
+                                self.selectors.append(tag + '.' + split_attrs[0] + '.' + split_attrs[1])
+                                self.selectors.append('.' + split_attrs[0])
+                                self.selectors.append('.' + split_attrs[1])
+                                self.selectors.append('.' + split_attrs[0] + '.' + split_attrs[1])
+                            else:
+                                self.selectors.append('.' + attrs[class_index][1])
+                                self.selectors.append(tag + '.' + attrs[class_index][1])
 
-                elif i[0] == 'id':
-                    id_index = attrs.index(i)
-                    
-                    if attrs and attrs[id_index][0] == 'id':
-                        if attrs and ' ' in attrs[id_index][1]:
-                            split_attrs = attrs[id_index][1].split()
-                            self.selectors.append(tag + '#' + split_attrs[0])
-                            self.selectors.append(tag + '#' + split_attrs[1])
-                            self.selectors.append(tag + '#' + split_attrs[0] + '#' + split_attrs[1])
-                            self.selectors.append('#' + split_attrs[0])
-                            self.selectors.append('#' + split_attrs[1])
-                            self.selectors.append('#' + split_attrs[0] + '#' + split_attrs[1])
-                        else:
-                            self.selectors.append('#' + attrs[id_index][1])
-                            self.selectors.append(tag + '#' + attrs[id_index][1])
-                else:
-                    self.selectors.append(tag)
+                    elif i[0] == 'id':
+                        id_index = attrs.index(i)
+                        
+                        if attrs and attrs[id_index][0] == 'id':
+                            if attrs and ' ' in attrs[id_index][1]:
+                                split_attrs = attrs[id_index][1].split()
+                                self.selectors.append(tag + '#' + split_attrs[0])
+                                self.selectors.append(tag + '#' + split_attrs[1])
+                                self.selectors.append(tag + '#' + split_attrs[0] + '#' + split_attrs[1])
+                                self.selectors.append('#' + split_attrs[0])
+                                self.selectors.append('#' + split_attrs[1])
+                                self.selectors.append('#' + split_attrs[0] + '#' + split_attrs[1])
+                            else:
+                                self.selectors.append('#' + attrs[id_index][1])
+                                self.selectors.append(tag + '#' + attrs[id_index][1])
+                    else:
+                        self.selectors.append(tag)
+            else:
+                self.selectors.append(tag)
 
 def compile_files(files_to_parse):
     file_list = files_to_parse.split(',')
@@ -61,76 +64,102 @@ def compile_files(files_to_parse):
     return new_file
 
 def css_parser(sheet):
-    css_dict = {}
+    css_dict = []
 
     for i_idx, i in enumerate(sheet.cssRules):
         if not isinstance(i, cssutils.css.CSSComment) and not isinstance(i, cssutils.css.CSSImportRule):
             for idx, j in enumerate(i.selectorList):
-                p = re.compile('\.\w*(\.)\w*')
-
-                if p.match(str(j.selectorText)) and ' ' not in j.selectorText:
-                    dot_combinator = re.split(('(\.\w*)(\.\w*)', str(j.selectorText)))
-                    
-                    for m in dot_combinator:
-                        if m == '':
-                            pass
-                        else:
-                            css_dict.update({m:[i, i_idx, i.selectorList, j, idx]})
-
-                elif ' ' in j.selectorText:
-                    combinator = str(j.selectorText)
-                    split_combinator = combinator.split()
-
-                    for k in split_combinator:
-                        if k == '>' or k == '+' or k == '~':
-                            pass
-                        elif p.match(k):
-                            dot_split = re.split('(\.\w*)(\.\w*)', k)
-                            for s in dot_split:
-                                if s == '':
-                                    pass
-                                else:
-                                    css_dict.update({s:[i, i_idx, i.selectorList, j, idx]})
-                        else:
-                            css_dict.update({k:[i, i_idx, i.selectorList, j, idx]})               
-                
-                elif '*' in j.selectorText:
-                    pass
-                else:
-                    css_dict.update({str(j.selectorText):[i, i_idx, i.selectorList, j, idx]})
+                css_dict.append((str(j.selectorText), [i, i_idx, i.selectorList, j, idx]))
 
     return css_dict
 
-def split_pseudo(d, sheet):
-    css_dict = {}
+def split_combinator(l, sheet):
+    css_dict = []
 
-    for key in d:
-        if ':' in key:
-            split_pseudo = key.split(':')
+    # print "L LENGTH:", len(l)
+
+    for i in l:
+        p = re.compile('\.\w+(-|\w)+\.\w+(-|\w)+')
+
+        if p.search(i[0]) and ' ' not in i[0]:
+            # print "INDEX if p.search", i[1][1], i[0]
+            dot_combinator = re.split('(\.\w+(-|\w)+)(\.\w+(-|\w)+)', i[0])
+
+            for j in dot_combinator:
+                if j == '' or len(j) <= 1:
+                    pass
+                else:
+                    # print "BEFORE for i in dot_combinator:", len(css_dict)
+                    # print "INDEX for i in dot_combinator:", i[1][1], i[0]
+                    css_dict.append((j, i[1]))
+                    # print "AFTER for j in dot_combinator:", len(css_dict)
+
+            # print "INDEX if ' ' not in key:", i[1][1], i[0]
+            # print "BEFORE if ' ' not in key:", len(css_dict)
+            css_dict.append((i[0], i[1]))
+            # print "AFTER if ' ' not in key:", len(css_dict)
+
+        elif ' ' in i[0]:
+            # print "INDEX elif ' ' in key:", i[1][1], i[0]
+
+            split_combinator = i[0].split()
+
+            for j in split_combinator:
+                if j == '>' or j == '+' or j == '~' or j == "*":
+                    # print "INDEX for j in split_combinator: if j == '>' or j == '+' or j == '~':", i[1][1], i[0]
+                    pass
+                elif p.search(j):
+                    dot_split = re.split('(\.\w+(-|\w)+)(\.\w+(-|\w)+)', j)
+
+                    for s in dot_split:
+                        if s == '' or len(s) <= 1:
+                            # print "INDEX for s in dot_split: if s == '' or len(s) <= 1", i[1][1], i[0]
+                            pass
+                        else:
+                            # print "INDEX for s in dot_split: else", i[1][1], i[0]
+                            # print "BEFORE for s in dot_split: else", len(css_dict)
+                            css_dict.append((s, i[1]))
+                            # print "AFTER for s in dot_split: else", len(css_dict)
+                else:
+                    # print "INDEX for j in split_combinator: else", i[1][1], i[0]
+                    # print "BEFORE for j in split_combinator: else", len(css_dict)
+                    css_dict.append((j, i[1]))
+                    # print "AFTER for j in split_combinator: else", len(css_dict) 
+
+        else:
+            # print "INDEX else:", i[1][1], i[0]
+            # print "BEFORE else:", len(css_dict)
+            css_dict.append((i[0], i[1]))
+            # print "AFTER else:", len(css_dict)
+
+
+    return css_dict
+
+def split_pseudo(l, sheet):
+    css_dict = []
+
+    for i in l:
+        if ':' in i[0]:
+            split_pseudo = i[0].split(':')
+
             if '[' not in split_pseudo[0]:
-                css_dict.update({split_pseudo[0]: css_parser(sheet).get(key)})
+                css_dict.append((split_pseudo[0], i[1]))
             else:
                 attribute_selector = split_pseudo[0].split('[')
-                css_dict.update({attribute_selector[0]: css_parser(sheet).get(key)})
+                css_dict.append((attribute_selector[0], i[1]))
+
         else:
-            css_dict.update({key: css_parser(sheet).get(key)})
+            css_dict.append((i[0], i[1]))
 
     return css_dict
 
 def del_dupes(l):
-    c = {}
     h = []
 
-    if type(l) is dict:
-        for key in l:
-            if key not in c:
-                c.update({key: l.get(key)})
-        return c
-    else:
-        for i in l:
-            if i not in h:
-                h.append(i)
-        return h
+    for i in l:
+        if i not in h:
+            h.append(i)
+    return h
 
 def delete_selectors(sheet, parser):
     rules_to_delete_index = []
@@ -138,30 +167,30 @@ def delete_selectors(sheet, parser):
     selectors_deleted = []
     ignore = ['body', 'div']
 
-    for key, value in del_dupes(split_pseudo(css_parser(sheet), sheet)).iteritems():
-        if key not in del_dupes(parser.selectors) and key not in ignore:
-            if len(value[2]) > 1:
-                rule_idx = value[1]
+    for i in split_pseudo(split_combinator(css_parser(sheet), sheet), sheet):
+        if i[0] not in del_dupes(parser.selectors) and i[0] not in ignore:
+            if len(i[1][2]) > 1:
+                rule_idx = i[1][1]
 
                 for idx, data in enumerate(sheet.cssRules[rule_idx].selectorList):
                     if idx != len(sheet.cssRules[rule_idx].selectorList) - 1:
                         pass
                     else:
-                        selectors_deleted.append(str(key) + ' from ' + str(value[2].selectorText))
+                        selectors_deleted.append(str(i[0]) + ' from ' + str(i[1][2].selectorText))
                         del sheet.cssRules[rule_idx].selectorList[idx]
             else:
-                rules_to_delete_text.append(str(value[2].selectorText))
-                rules_to_delete_index.append(value[1])
+                rules_to_delete_text.append(str(i[1][2].selectorText))
+                rules_to_delete_index.append(i[1][1])
 
     return (rules_to_delete_index, selectors_deleted, rules_to_delete_text)
 
 def delete_rules(rules, sheet, parser):
-    rules = delete_selectors(sheet, parser)[0]
+    rules = del_dupes(delete_selectors(sheet, parser)[0])
 
     rules.sort()
     rules.reverse()
     
     for i in rules:
-        sheet.deleteRule(i)
+        del sheet.cssRules[i]
 
-    return sheet.cssText 
+    return sheet.cssText
