f834153e7ca8a4bfe6522811fc81bd6e5b1aa65d
Jona Haga-Camacho <jona.haga@gmail.com>
2014-03-31 21:25:02 -0700

    Added flash messages if urls are entered wrong

--- views.py
+++ views.py
@@ -1,8 +1,9 @@
 from cssclean import *
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 
 app = Flask(__name__)
+app.secret_key = "shhhhthisisasecret"
 
 @app.route("/", methods=["POST"])
 def process():
@@ -17,23 +18,27 @@ def results():
     html_to_parse = request.form.get("html")
     css_to_parse = request.form.get("css")
 
-    # instantiate the parser and feed it some HTML
-    parser = MyHTMLParser()
-    parser.feed(compile_html(html_to_parse))
-    # parser.feed(compile_css(html_to_parse))
-    parser.close()
+    if 'http' not in html_to_parse or 'http' not in css_to_parse:
+        flash("Don't forget 'http://'")
+        return redirect(url_for("index"))
+    else:
+        # instantiate the parser and feed it some HTML
+        parser = MyHTMLParser()
+        parser.feed(compile_html(html_to_parse))
+        # parser.feed(compile_css(html_to_parse))
+        parser.close()
 
-    # Run delete_rules function to delete unused rules and get new stylesheet
-    sheet = cssutils.CSSParser().parseString(compile_css(css_to_parse))
-    deleted_selectors = delete_selectors(sheet, parser)[2]
-    deleted_rules = del_dupes(delete_selectors(sheet, parser)[1])
-    new_stylesheet = delete_rules(delete_selectors, sheet, parser)
+        # Run delete_rules function to delete unused rules and get new stylesheet
+        sheet = cssutils.CSSParser().parseString(compile_css(css_to_parse))
+        deleted_selectors = delete_selectors(sheet, parser)[2]
+        deleted_rules = del_dupes(delete_selectors(sheet, parser)[1])
+        new_stylesheet = delete_rules(delete_selectors, sheet, parser)
 
-    deleted_rules.sort()
+        deleted_rules.sort()
 
-    return render_template("results.html", deleted_selectors=deleted_selectors,
-                                           deleted_rules=deleted_rules,
-                                           new_stylesheet=new_stylesheet )
+        return render_template("results.html", deleted_selectors=deleted_selectors,
+                                               deleted_rules=deleted_rules,
+                                               new_stylesheet=new_stylesheet )
 
 if __name__ == "__main__":
     app.run(debug=True)
