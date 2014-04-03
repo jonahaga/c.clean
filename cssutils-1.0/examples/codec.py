3bf0ba63c55b4a0e55b57adc212605e3a45fb6f9
Jona Haga-Camacho <jona.haga@gmail.com>
2014-04-03 13:39:26 -0700

    Removed unused directory

--- cssutils-1.0/examples/codec.py
+++
@@ -1,9 +0,0 @@
-"""codec usage example
-"""
-import codecs
-import cssutils
-
-cssText = codecs.open('../sheets/cases.css', encoding='css').read()
-sheet = cssutils.parseString(cssText)
-print sheet
-print sheet.cssText
