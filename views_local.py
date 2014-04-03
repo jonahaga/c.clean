cd6b1aa50ba5458b3c829a9b22c1ea5efd7178c9
Jona Haga-Camacho <jona.haga@gmail.com>
2014-04-03 13:30:53 -0700

    View file for local testing that doesn't check for 'http://'

---
+++ views_local.py
@@ -0,0 +1,60 @@
+from cssclean import *
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+app.secret_key = "shhhhthisisasecret"
+
+@app.route("/", methods=["POST"])
+def process():
+    return render_template("results.html")
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+@app.route("/results", methods=["POST"])
+def results():
+    html_to_parse = request.form.get("html")
+    css_to_parse = request.form.get("css")
+    phantom = request.form.get("phantom")
+
+    # instantiate the parser and feed it some HTML
+    parser = MyHTMLParser()
+
+    if phantom == "without_phantom":
+        parser.feed(compile_files(html_to_parse))
+        parser.close()
+
+        # Run delete_rules function to delete unused rules and get new stylesheet
+        sheet = cssutils.CSSParser().parseString(compile_files(css_to_parse))
+        deleted_selectors = delete_selectors(sheet, parser)[2]
+        deleted_rules = del_dupes(delete_selectors(sheet, parser)[1])
+        new_stylesheet = delete_rules(delete_selectors, sheet, parser)
+
+        deleted_rules.sort()
+
+        return render_template("results.html", deleted_selectors=deleted_selectors,
+                                               deleted_rules=deleted_rules,
+                                               new_stylesheet=new_stylesheet )
+    else:
+        parser.feed(compile_phantom(html_to_parse))
+        parser.close()
+
+        # Run delete_rules function to delete unused rules and get new stylesheet
+        orig_stylesheet = cssutils.CSSParser().parseString(compile_files(css_to_parse)).cssText
+        sheet = cssutils.CSSParser().parseString(compile_files(css_to_parse))
+        deleted_selectors = delete_selectors(sheet, parser)[2]
+        deleted_rules = del_dupes(delete_selectors(sheet, parser)[1])
+        new_stylesheet = delete_rules(delete_selectors, sheet, parser)
+        sheet_diff = diff(orig_stylesheet, new_stylesheet)
+
+        deleted_rules.sort()
+
+        return render_template("results.html", deleted_selectors=deleted_selectors,
+                                               deleted_rules=deleted_rules,
+                                               new_stylesheet=new_stylesheet,
+                                               sheet_diff=sheet_diff )
+
+if __name__ == "__main__":
+    app.run(debug=True)
